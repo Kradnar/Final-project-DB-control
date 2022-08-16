d91774e1393ccb6ee94945667f5a93dd245e7635
Kradnar <pete.daniel.1975@gmail.com>
2022-08-16 12:46:30 +0100

    v0.11

--- src/server.js
+++ src/server.js
@@ -4,7 +4,25 @@ const cors = require("cors");
 
 const userRouter = require("./user/routes");
 const movieRouter = require("./movie/routes");
+
+const port = process.env.PORT || 5000
+
 const app = express();
+
+//! \/  \/ Possible problems with hosting
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  parameterLimit: 100000,
+  extended: true 
+}));
+//! /\  /\ Possible problems with hosting
+
 
 //add relevant routes and controllers to app before listen runs
 app.use(express.json()); // Tell entire server that it will always recieve JSON, and to always send back JSON
@@ -12,6 +30,11 @@ app.use(cors());
 app.use(userRouter, movieRouter);
 
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
+app.get("/health", (req, res) => {
+  res.status(200).send({ message: "API is working" });
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
